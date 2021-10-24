bdb6f6e2498205233f0470659c05a30657b0ab2f
Daniel Andrade <danielandradesilva.84@gmail.com>
2021-10-23 21:20:57 -0300

    Changing theme

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -13,7 +13,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course.";
+            ViewData["Email"] = "daniel@email.com.br";
+            ViewData["Professor"] = "Nélio Alves.";
 
             return View();
         }
